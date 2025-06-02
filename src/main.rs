6a6689a0a56333b4b07697f7c92ce98907588099
Pawan <pawanbajaj2012@gmail.com>
2025-06-02 14:55:34 +0800

    feat: add leader elections

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,37 @@
 mod raft;
-use raft::RaftNode;
+use std::{collections::HashMap};
+
+use raft::{Message, RaftNode};
+use tokio::{sync::mpsc::unbounded_channel, task};
 
 #[tokio::main]
-async fn main () {
-    let mut node = RaftNode::new(1);
-    node.run().await;
+async fn main() {
+    let node_ids = vec![1,2,3];
+    let mut tx_map: HashMap<u64, tokio::sync::mpsc::UnboundedSender<Message>> = HashMap::new();
+    let mut rx_map: HashMap<u64, tokio::sync::mpsc::UnboundedReceiver<Message>> = HashMap::new();
+
+    // create channels for each node
+    for &id in &node_ids {
+        let (tx, rx) = unbounded_channel();
+        tx_map.insert(id, tx);
+        rx_map.insert(id, rx);
+    }
+    
+    // spawn tasks for each node
+    for &id in &node_ids {
+        let peers: Vec<u64> = node_ids.iter().copied().filter(|x| *x != id).collect();
+        let node_tx_map = tx_map.clone();
+        let node_rx = rx_map.remove(&id).unwrap();
+
+        task::spawn(async move {
+            let mut node = RaftNode::new(id, peers, node_tx_map, node_rx);
+            node.run().await;
+        });
+    }
+    // sleep forever to keep main alive
+    println!("🔧 Spinning up Raft cluster...");
+
+    loop {
+        tokio::time::sleep(tokio::time::Duration::from_secs(60)).await;
+    }
 }
--- src/raft.rs
+++ src/raft.rs
@@ -1,7 +1,8 @@
 use rand::Rng;
+use std::collections::HashMap;
 use std::time::{Duration, Instant};
+use tokio::sync::mpsc::{unbounded_channel, UnboundedReceiver, UnboundedSender};
 use tokio::time::sleep;
-
 
 #[derive(Debug, Clone, PartialEq)]
 enum Role {
@@ -17,59 +18,165 @@ pub struct RaftNode {
     voted_for: Option<u64>,
     election_timeout: Duration,
     last_heartbeat: Instant,
+
+    peers: Vec<u64>,                                // ids of other nodes
+    tx_map: HashMap<u64, UnboundedSender<Message>>, // senders to peers
+    rx: UnboundedReceiver<Message>,                 // receiver for incoming messages
 }
 
 impl RaftNode {
-    pub fn new(id: u64) -> Self {
+    pub fn new(
+        id: u64,
+        peers: Vec<u64>,
+        tx_map: HashMap<u64, UnboundedSender<Message>>,
+        rx: UnboundedReceiver<Message>,
+    ) -> Self {
         // random election timeout between 150 and 300 ms
         let mut rng = rand::thread_rng();
-        let timeout_ms = rng.gen_range(150..=300);
-
+        let timeout_ms = rng.gen_range(300..=500);
         Self {
             id,
             role: Role::Follower,
             current_term: 0,
             voted_for: None,
             election_timeout: Duration::from_millis(timeout_ms),
-            last_heartbeat: Instant::now()
+            last_heartbeat: Instant::now(),
+
+            peers,
+            tx_map,
+            rx,
         }
     }
 
     pub async fn run(&mut self) {
         loop {
-            match self.role {
-                Role::Follower | Role::Candidate => {
-                    let elapsed = self.last_heartbeat.elapsed();
-                    if elapsed >= self.election_timeout {
-                        println!("Node {} election timeout expired", self.id);
-                        self.start_election().await;
-                    } else {
-                        sleep(self.election_timeout - elapsed).await;
+            tokio::select! {
+                Some(msg) = self.rx.recv() => {
+                    self.handle_message(msg).await;
+                } _ = sleep(Duration::from_millis(100)) => {
+                    match self.role {
+                        Role::Follower | Role::Candidate => {
+                            let elapsed = self.last_heartbeat.elapsed();
+                            if elapsed >= self.election_timeout {
+                                println!("Node {} election timeout expired", self.id);
+                                self.start_election().await;
+                            } else {
+                                sleep(self.election_timeout - elapsed).await;
+                            }
+                        }
+                        Role::Leader => {
+                            println!("Node {} sending heartbeats", self.id);
+                            // for now just wait before sending next heartbeat
+                            sleep (Duration::from_millis(50)).await;
+                        }
                     }
-                }
-                Role::Leader => {
-                    println!("Node {} sending heartbeats", self.id);
-                    // for now just wait before sending next heartbeat
-                    sleep (Duration::from_millis(50)).await;
                 }
             }
         }
-    } 
+    }
+
+    async fn handle_message(&mut self, msg: Message) {
+        match msg {
+            Message::RequestVote {
+                term,
+                candidate_id,
+                respond_to,
+            } => {
+                if term > self.current_term {
+                    self.current_term = term;
+                    self.role = Role::Follower;
+                    self.voted_for = None;
+                }
+
+                let vote_granted = if term == self.current_term
+                    && (self.voted_for.is_none() || self.voted_for == Some(candidate_id))
+                {
+                    self.voted_for = Some(candidate_id);
+                    true
+                } else {
+                    false
+                };
+
+                //  reply with vote decision
+                let _ = respond_to.send(Message::RequestVoteResponse { term: self.current_term, vote_granted: vote_granted, voted_id: self.id });
+            }
+            Message::RequestVoteResponse { term, vote_granted, voted_id } => {
+                if term > self.current_term {
+                    self.current_term = term;
+                    self.role = Role::Follower;
+                    self.voted_for = None;
+                    return;
+                }
+
+                if self.role == Role::Candidate && vote_granted {
+                    println!("Node {} received vote from {}", self.id, voted_id);
+                    // TODO: track votes and become leader if majority reached
+                }
+            }
+        }
+    }
+
     async fn start_election(&mut self) {
+        println!("🚀 Node {} starting election for term {}", self.id, self.current_term + 1);
         self.role = Role::Candidate;
         self.current_term += 1;
         self.voted_for = Some(self.id);
         self.last_heartbeat = Instant::now();
 
-        println!("NOde {} starting election for term {}", self.id, self.current_term);
+        println!(
+            "NOde {} starting election for term {}",
+            self.id, self.current_term
+        );
 
-        // TODO: Request votes from peers here
-        // for now become leader immediately to keep it simple
-        self.become_leader();
+        let mut votes = 1;
+        let majority = (self.peers.len() + 1) / 2 + 1;
+
+        for peer_id in &self.peers {
+            if let Some(tx) = self.tx_map.get(peer_id) {
+                let (resp_tx, mut resp_rx) = unbounded_channel();
+                let _ = tx.send(Message::RequestVote { term: self.current_term, candidate_id: self.id, respond_to: resp_tx });
+
+                // await single response with timeout
+                if let Ok(Some(Message::RequestVoteResponse {vote_granted, ..})) = 
+                    tokio::time::timeout(Duration::from_millis(200), resp_rx.recv()).await {
+                        if vote_granted {
+                            votes += 1;
+                            if votes >= majority {
+                                self.become_leader();
+                                break;
+                            }
+                        }
+                    }
+            }
+        }
+
+        if self.role == Role::Candidate {
+            println!("Node {} failed to get majority votes", self.id);
+            // back to follower
+            self.role = Role::Follower;
+            self.voted_for = None;
+        }
     }
 
     fn become_leader(&mut self) {
         self.role = Role::Leader;
-        println!("Node {} is now the leader for term {}", self.id, self.current_term);
+        println!(
+            "Node {} is now the leader for term {}",
+            self.id, self.current_term
+        );
     }
+}
+
+#[derive(Debug)]
+pub enum Message {
+    RequestVote {
+        term: u64,
+        candidate_id: u64,
+        respond_to: UnboundedSender<Message>,
+    },
+    RequestVoteResponse {
+        term: u64,
+        vote_granted: bool,
+        voted_id: u64,
+    },
 }
